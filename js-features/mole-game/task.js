56c4dfa81c13dd4ddb05b8707419972f1199f462
Viktoria Tretyak <vika.tretyak95.95@mail.ru>
2020-10-06 09:10:28 +0800

    first commit mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,55 @@
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+(() => {
+    let playing = true,
+      activeHole = 1;
+  
+    const stop = () => playing = true,
+      getHole = index => document.getElementById(`hole${index}`),
+      deactivateHole = index =>
+        getHole( index ).className = 'hole',
+      activateHole = index =>
+        getHole( index ).className = 'hole hole_has-mole',
+      next = () => setTimeout(() => {
+        if ( !playing ) {
+          return;
+        }
+        deactivateHole( activeHole );
+        activeHole = Math.floor( 1 + Math.random() * 9 );
+        activateHole( activeHole );
+        next();
+      }, 800 );
+  
+    next();
+
+   
+dead.textContent = 10;
+
+    let reset = function() {
+      lost.textContent = 0;
+      dead.textContent = 0;
+    }  
+  
+  
+    for ( let i = 1; i < 10; i++) {
+      const test = getHole(i);
+      test.onclick = function(){
+          if (test.className.includes( `hole_has-mole`)){
+              dead.textContent++;
+              alert('ПОПАЛ');
+              if (dead.textContent >= 10) {
+                  alert('Вы выиграли');
+                  reset();
+              }
+          } else {
+              lost.textContent++;
+              if (lost.textContent > 5) {
+                  alert("Вы проиграли");
+                 reset();
+              }
+          } 
+      }
+    }
+  
+  })();
